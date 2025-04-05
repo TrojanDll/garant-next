3b818d75a67689a08387723ea147c6bc9cd1a326
Timur <60787064+TrojanDll@users.noreply.github.com>
2025-04-05 14:00:30 +0300

    find some select classes

--- src/components/features/CalculatorInputForm/CalculatorInputForm.tsx
+++ src/components/features/CalculatorInputForm/CalculatorInputForm.tsx
@@ -1,22 +1,31 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const Select = dynamic(() => import("react-select"), {
+  ssr: false,
+});
+
 import React, { useRef } from "react";
-import { Form, FormProps, Input, Select } from "antd";
 
 import styles from "./CalculatorInputForm.module.scss";
+
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
 
 const CalculatorInputForm = () => {
   return (
     <Select
-        className="basic-single"
-        classNamePrefix="selectskdbvskdv"
-        defaultValue={colourOptions[0]}
-        isDisabled={isDisabled}
-        isLoading={isLoading}
-        isClearable={isClearable}
-        isRtl={isRtl}
-        isSearchable={isSearchable}
-        name="color"
-        options={colourOptions}
-      />
+      placeholder="Выберите категорию ТС"
+      classNames={{
+        control: () => styles.control,
+        placeholder: () => styles.placeholder
+      }}
+      options={options}
+    />
   );
 };
 
