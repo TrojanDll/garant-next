ee48773a02cfae4a91b19a0dfa16401ed1bcf2bb
Timur <60787064+TrojanDll@users.noreply.github.com>
2025-04-17 20:39:42 +0300

    fix

---
+++ src/hooks/auth/useLogin.ts
@@ -0,0 +1,32 @@
+import { authService } from "@/services/auth.service";
+import { ILoginForm } from "@/types/auth.types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
+export function useLogin() {
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: login,
+    isPending: isLoginPending,
+    data: loginResponse,
+    isSuccess: isLoginSuccess,
+    isError: isLoginError,
+  } = useMutation({
+    mutationKey: ["login"],
+    mutationFn: (data: ILoginForm) => authService.login(data),
+    onSuccess() {
+      queryClient.invalidateQueries({
+        queryKey: ["login"],
+      });
+    },
+  });
+
+  return {
+    login,
+    isLoginPending,
+    loginResponse,
+    isLoginSuccess,
+    isLoginError,
+  };
+}
