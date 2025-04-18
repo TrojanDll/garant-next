ebcb3099bc69b8ac1e530cd1e906c9f6a64c2413
Timur <60787064+TrojanDll@users.noreply.github.com>
2025-04-18 21:17:19 +0300

    fix: next config proxy

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,17 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// next.config.js
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", 
+        destination: "https://https://xn----nbck7b7ald8atlv.xn--y9a3aq/strahovanie.loc/public/:path*",
+      },
+    ];
+  },
+};
+
+
 export default nextConfig;
