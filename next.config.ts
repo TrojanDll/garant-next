3bd3fca2b882dbe176d327f444464a16ecf0d80d
Timur <60787064+TrojanDll@users.noreply.github.com>
2025-04-18 21:32:12 +0300

    fix: next.config rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-// next.config.js
-module.exports = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*", 
-        destination: "https://https://xn----nbck7b7ald8atlv.xn--y9a3aq/strahovanie.loc/public/:path*",
+        source: "/api/:path*",
+        destination: "https://xn----nbck7b7ald8atlv.xn--y9a3aq/strahovanie.loc/public/:path*",
       },
     ];
   },
 };
-
 
 export default nextConfig;
