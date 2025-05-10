af24d116bc844cbb53239ee81be14faf8e187abc
Timur <60787064+TrojanDll@users.noreply.github.com>
2025-05-10 12:31:25 +0300

    feat: Support desktop

--- src/components/widgets/Support/Support.tsx
+++ src/components/widgets/Support/Support.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import styles from "./Support.module.scss";
 
 import Substrate from "@/components/ui/Substrate/Substrate";
-import CustomTitle from "@/components/ui/CustomTitle/CustomTitle";
 import ContentContainer from "@/components/ui/ContentContainer/ContentContainer";
 import FAQ from "../FAQ/FAQ";
 import Text from "@/components/ui/Text/Text";
 import Link from "next/link";
+import SocialLinks from "@/components/ui/SocialLinks/SocialLinks";
 
 const Support = () => {
   return (
@@ -24,6 +24,8 @@ const Support = () => {
           <Link href="tel:79409901234" className={styles.phone}>
             +7 940 990 12 34
           </Link>
+
+          <SocialLinks className={styles.socialLinks} />
         </Substrate>
       </ContentContainer>
     </section>
