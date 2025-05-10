884d4abad1e3012a53d0b1bdf1540c1e1e531736
Timur <60787064+TrojanDll@users.noreply.github.com>
2025-05-10 23:33:28 +0300

    test

--- src/components/widgets/MyPolicies/MyPolicies.tsx
+++ src/components/widgets/MyPolicies/MyPolicies.tsx
@@ -13,6 +13,7 @@ const MyPolicies = () => {
   const { data, isError, isLoading, isSuccess } = useGetPoliciesByCurrientUser();
 
   // test
+  // test
   return (
     <div>
       <CustomTitle isCentered className={styles.title}>
