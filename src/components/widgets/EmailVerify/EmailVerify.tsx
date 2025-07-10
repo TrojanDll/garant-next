192a967addde866f9c366449ea18bf13afb1557f
Timur <60787064+TrojanDll@users.noreply.github.com>
2025-07-10 11:51:54 +0300

    feat: redirection after email confirm

--- src/components/widgets/EmailVerify/EmailVerify.tsx
+++ src/components/widgets/EmailVerify/EmailVerify.tsx
@@ -6,6 +6,10 @@ import Substrate from "@/components/ui/Substrate/Substrate";
 import { useParams, useSearchParams } from "next/navigation";
 import CustomTitle from "@/components/ui/CustomTitle/CustomTitle";
 import { useEffect, useState } from "react";
+import Text from "@/components/ui/Text/Text";
+import CustomLink from "@/components/ui/CustomLink/CustomLink";
+import { PAGES } from "@/config/pages-url.config";
+import { useNavigation } from "@/hooks/navigation/useNavigation";
 
 export default function EmailVerify() {
   const [isVerified, setIsVerified] = useState<boolean>(false);
@@ -14,21 +18,44 @@ export default function EmailVerify() {
 
   const params = useSearchParams();
 
+  const { navigateToDashboard } = useNavigation();
+
   useEffect(() => {
     setIsVerified(params.get("verified") === "true" ? true : false);
     setMessage(params.get("message"));
     setIsProcessed(true);
   }, []);
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      navigateToDashboard();
+    }, 4000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   function confirmationTitle(): string {
     if (isProcessed) {
       if (isVerified) {
-        return "Почта успешно подтверждена";
+        return "Почта успешно подтверждена!";
       } else {
         return "Ошибка подтверждения почты";
       }
     }
 
+    return "";
+  }
+
+  function confirmationDescription(): string {
+    if (isProcessed) {
+      if (isVerified) {
+        return "Вы будете автоматически направлены в личный кабинет. Либо нажмите на ссылку ниже.";
+      } else {
+        return "Что-то пошло не так при подтверждении почты.";
+      }
+    }
     return "";
   }
 
@@ -38,6 +65,18 @@ export default function EmailVerify() {
         <CustomTitle tag="h2" isCentered>
           {confirmationTitle()}
         </CustomTitle>
+
+        <Text className={styles.description}>{confirmationDescription()}</Text>
+
+        {isVerified && (
+          <CustomLink
+            href={PAGES.DASHBOARD}
+            variant="underline"
+            className={styles.redirectLink}
+          >
+            Личный кабинет
+          </CustomLink>
+        )}
       </Substrate>
     </ContentContainer>
   );
