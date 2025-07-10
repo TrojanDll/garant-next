befa0712a05a35df6de9e07fa2896f26cae680d7
Timur <60787064+TrojanDll@users.noreply.github.com>
2025-07-10 12:57:02 +0300

    csrf token

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -4,8 +4,10 @@ import { errorCatch } from "./error";
 import { getToken, removeFromStorage } from "@/services/auth-token.service";
 import { PAGES } from "@/config/pages-url.config";
 
+const BASE_URL = "https://garantcp.ru";
+
 const options: CreateAxiosDefaults = {
-  baseURL: "https://garantcp.ru",
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
@@ -15,15 +17,32 @@ const options: CreateAxiosDefaults = {
 const axiosClassic = axios.create(options);
 const axiosWithAuth = axios.create(options);
 
-axiosWithAuth.interceptors.request.use((config) => {
-  const token = getToken();
+// Добавляем CSRF-интерцептор для axiosClassic (для неавторизованных запросов)
+axiosClassic.interceptors.request.use(async (config) => {
+  // Пропускаем для самого запроса за CSRF-токеном
+  if (config.url?.includes("sanctum/csrf-cookie")) return config;
 
-  // if (config?.headers && token)
-  //   config.headers.Authorization = `Bearer ${token}`;
-
+  // Для всех остальных запросов сначала получаем CSRF
+  await axios.get(`${BASE_URL}/sanctum/csrf-cookie`, {
+    withCredentials: true,
+  });
   return config;
 });
 
+axiosWithAuth.interceptors.request.use(async (config) => {
+  // const token = getToken();
+
+  // Получаем CSRF-токен перед авторизованными запросами
+  await axios.get(`${BASE_URL}/sanctum/csrf-cookie`, {
+    withCredentials: true,
+  });
+
+  // if (config?.headers && token) {
+  //   config.headers.Authorization = `Bearer ${token}`;
+  // }
+
+  return config;
+});
 axiosWithAuth.interceptors.response.use(
   (config) => config,
   async (error) => {
@@ -33,7 +52,6 @@ axiosWithAuth.interceptors.response.use(
       errorCatch(error) === "Unauthenticated."
     ) {
       // removeFromStorage();
-
       // if (typeof window !== "undefined") {
       //   window.location.href = PAGES.AUTH;
       // }
