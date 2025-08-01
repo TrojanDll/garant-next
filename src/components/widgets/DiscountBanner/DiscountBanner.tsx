2763c1ae374b20c751365b91e08df0596e6ecae0
Alexander Ketsba <140740007+Sandrobus228@users.noreply.github.com>
2025-08-01 09:14:31 +0300

    new promocode

--- src/components/widgets/DiscountBanner/DiscountBanner.tsx
+++ src/components/widgets/DiscountBanner/DiscountBanner.tsx
@@ -25,7 +25,7 @@ const DiscountBanner = () => {
             </div>
 
             <div className={styles.textWrapper}>
-              <p className={styles.descr}>при оформлении до 31 июля</p>
+              <p className={styles.descr}>при оформлении до 31 августа</p>
             </div>
           </div>
         </div>
