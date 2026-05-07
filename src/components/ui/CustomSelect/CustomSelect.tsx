630bdbafcefc53a6e05c4ba3a823f8501feb5b5a
Alexander Ketsba <140740007+Sandrobus228@users.noreply.github.com>
2026-05-08 02:53:06 +0300

    Update CustomSelect.tsx

--- src/components/ui/CustomSelect/CustomSelect.tsx
+++ src/components/ui/CustomSelect/CustomSelect.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import dynamic from "next/dynamic";
-const Select = dynamic(() => import("react-select"), {
-  ssr: false,
-});
+import Select from "react-select";
 
 import Tooltip from "../Tooltip/Tooltip";
 
@@ -257,6 +254,7 @@ const CustomSelect = ({
               }
             : null
         }
+        instanceId={name}
         filterOption={() => true}
         name={name}
         inputValue={inputValue}
